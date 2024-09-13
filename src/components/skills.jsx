eef144011f7b78e29f466e41c73dc5e80818f4e4
Caleb <2021.cee.jay@gmail.com>
2024-09-13 11:25:36 +0100

    "Updated styles for Skills component: adjusted padding and width for skill items"

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,7 +2,7 @@ import { skillsList } from "../constants";
 const Skills = () => {
   return (
     <section className="lg:py-[60px] py-10 px-4 lg:px-[80px]" id="skills">
-      <div className="flex flex-col items-center px-8 gap-5">
+      <div className="flex flex-col items-center gap-5 lg:px-8">
         <h2 className="font-normal md:text-[48px] text-[28px]">
           My <span className="font-extrabold">Skills</span>
         </h2>
@@ -10,7 +10,7 @@ const Skills = () => {
         <div className="py-5 flex items-center gap-5 lg:gap-[71px] flex-wrap justify-center">
           {skillsList.map((skill, index) => {
             return (
-              <div key={index} className="flex flex-col gap-8 items-center hover:bg-[var(--black)] transition-all hover:text-white justify-center lg:w-[186px] w-[160px] h-[160px] lg:h-[186px] rounded border-2 p-6 border-[var(--black)]">
+              <div key={index} className="flex flex-col gap-8 items-center hover:bg-[var(--black)] transition-all hover:text-white justify-center lg:w-[186px] w-[150px] h-[150px] lg:h-[186px] rounded border-2 p-6 border-[var(--black)]">
                 <i className={`${skill.icon} text-[40px]`}></i>
                 <span className="text-lg">{skill.title}</span>
               </div>
