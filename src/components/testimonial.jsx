fede33b7959162d02e88767dad0a756f4bb36c09
Caleb <2021.cee.jay@gmail.com>
2024-09-14 11:06:14 +0100

    "Update font size of h2 element in Testimonial component"

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -5,7 +5,7 @@ const Testimonial = () => {
     <div className="sm:py-[60px] sm:px-10 py-10 px-4">
       <div className="flex flex-col items-center px-8 sm:gap-5">
         <div className="py-5">
-          <h2 className="font-normal text-[48px]">
+          <h2 className="font-normal text-[28px] sm:text-[48px]">
             My <span className="font-extrabold">Testimonial</span>
           </h2>
         </div>
