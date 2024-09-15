e4d39d93311d3dd358bd2c2c616eeb7d41cfa027
Caleb <2021.cee.jay@gmail.com>
2024-09-15 23:25:20 +0100

    Added formspree

--- src/components/templates/form.jsx
+++ src/components/templates/form.jsx
@@ -1,27 +1,31 @@
 import SocialLinks from "../common/socialLinks";
 const ContactForm = () => {
     return ( 
-        <form action="" className="flex flex-col gap-5">
+        <form action="https://formspree.io/f/movazpbw" method="POST" className="flex flex-col gap-5">
             <input
               type="text"
               placeholder="Your name"
               required
+              name="Name"
               className="xl:w-[500px] w-full h-[56px] hover:border-[var(--neutral)] focus:shadow-focusShadow hover:bg-[var(--zinc-100)] py-4 rounded outline-none px-6 border-[1.4px] border-black"
             />
             <input
               type="email"
               required
               placeholder="Email"
+              name="Email"
               className="xl:w-[500px] w-full h-[56px] hover:border-[var(--neutral)] focus:shadow-focusShadow hover:bg-[var(--zinc-100)] py-4 rounded outline-none px-6 border-[1.4px] border-black"
             />
             <input
               type="url"
               placeholder="Your website (If exists [https://]) "
+              name="Website"
               className="xl:w-[500px] w-full h-[56px] hover:border-[var(--neutral)] focus:shadow-focusShadow hover:bg-[var(--zinc-100)] py-4 rounded outline-none px-6 border-[1.4px] border-black"
             />
             <textarea
               placeholder="How can I help?*"
               required
+              name="Message"
               className="resize-none xl:w-[500px] w-full h-[140px] hover:border-[var(--neutral)] focus:shadow-focusShadow hover:bg-[var(--zinc-100)] py-4 rounded outline-none px-6 border-[1.4px] border-black"
             />
             <div className="flex flex-wrap items-center gap-6">
