118de903a31961b6d4cca4dbd998a7d7f0bbc425
Caleb <2021.cee.jay@gmail.com>
2024-09-15 23:37:30 +0100

    "Updated Footer component styles and layout"

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,16 @@
 import logo from "/logo.svg"
 const Footer = () => {
     return ( 
-        <div className="px-20 py-6 bg-[var(--black)] text-[var(--white)]">
-            <div className="flex items-center justify-between px-8">
+        <div className="md:px-20 md:py-6  py-5 px-4 bg-[var(--black)] text-[var(--white)]">
+            <div className="flex items-center justify-between md:px-8">
                 <div className="flex items-center gap-3">
-                    <img src={logo} alt="logo white" />
-                    <h2  className="font-bold text-[20px]">Personal</h2>
+                    <img src={logo} alt="logo white" className="w-[27px] h-[28px]"/>
+                    <h2  className="font-bold sm:text-[20px] text-[17px]">Personal</h2>
                 </div>
 
                 <div>
-                    <p className="font-semibold">@ 2019-2023 Personal</p>
-                    <p className="font-semibold">Cee Jay</p>
+                    <p className="font-medium md:font-semibold md:text-[16px] text-[12px]">@ 2019-2023 Personal</p>
+                    <p className="font-medium md:font-semibold md:text-[16px] text-[12px]">Cee Jay</p>
                 </div>
             </div>
         </div>
