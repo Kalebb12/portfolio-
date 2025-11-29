5e2d85415fa757263776ab44274fbe59cfa501db
Caleb <justcaleb01@gmail.com>
2025-11-29 23:41:02 +0100

    Update logo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,20 +1,15 @@
-import logo from "/logo.svg"
 const Footer = () => {
-    return ( 
-        <div className="md:px-20 md:py-6  py-5 px-4 bg-[var(--black)] text-[var(--white)]">
-            <div className="container flex items-center justify-between mx-auto md:px-8">
-                <div className="flex items-center gap-3">
-                    <img src={logo} alt="logo white" className="w-[27px] h-[28px]"/>
-                    <h2  className="font-bold sm:text-[20px] text-[17px]">Personal</h2>
-                </div>
+  return (
+    <div className="md:px-20 md:py-6  py-5 px-4 bg-[var(--black)] text-[var(--white)]">
+      <div className="container flex items-center justify-between mx-auto md:px-8">
+        <h2 className="text-2xl font-bold">&lt; Caleb &gt;</h2>
 
-                <div>
-                    <p className="font-medium md:font-semibold md:text-[16px] text-[12px]">@ 2019-2023 Personal</p>
-                    <p className="font-medium md:font-semibold md:text-[16px] text-[12px]">Cee Jay</p>
-                </div>
-            </div>
-        </div>
-     );
-}
- 
+        <p className="font-medium flex items-center gap-2 md:font-semibold md:text-[16px] text-[12px]">
+          <span>Made with</span><img src="/heart.svg" alt="love." />
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
